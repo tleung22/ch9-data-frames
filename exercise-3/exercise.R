f4faf53d860099b6344672cb7f065abbc8740e3a
tleung22 <trevor22@uw.edu>
2018-04-13 14:45:49 -0700

    Add classwork for exercises

--- exercise-3/exercise.R
+++ exercise-3/exercise.R
@@ -2,35 +2,35 @@
 
 # Load R's "USPersonalExpenditure" dataset using the `data()` function
 # This will produce a data frame called `USPersonalExpenditure`
-
+data("USPersonalExpenditure")
 
 # The variable `USPersonalExpenditure` is now accessible to you. Unfortunately,
 # it's not a data frame (it's actually what is called a matrix)
 # Test this using the `is.data.frame()` function
-
+is.data.frame(USPersonalExpenditure)
 
 # Luckily, you can pass the USPersonalExpenditure variable as an argument to the
 # `data.frame()` function to convert it a data farm. Do this, storing the
 # result in a new variable
-
+us_exp <- data.frame(USPersonalExpenditure)
 
 # What are the column names of your dataframe?
-
+colnames(us_exp)
 
 ## Consider: why are they so strange? Think about whether you could use a number 
 ## like 1940 with dollar notation!
 
 # What are the row names of your dataframe?
-
+rownames(us_exp)
 
 # Add a column "category" to your data frame that contains the rownames
-
+us_exp$category <- rownames(us_exp)
 
 # How much money was spent on personal care in 1940?
-
+us_exp["Personal Care", "X1940"]
 
 # How much money was spent on Food and Tobacco in 1960?
-
+us_exp["Food and Tobacco", "X1940"]
 
 # What was the highest expenditure category in 1960?
 
--- exercise-5/exercise.R
+++ exercise-5/exercise.R
@@ -2,29 +2,36 @@
 
 # Read in the female baby names data file found in the `data` folder into a 
 # variable called `names`. Remember to NOT treat the strings as factors!
-
+names <- read.csv("data/female_names.csv", stringsAsFactors = FALSE)
 
 # Create a data frame `names_2013` that contains only the rows for the year 2013
-
+names_2013 <- data.frame(names[names$year == "2013", ])
 
 # What was the most popular female name in 2013?
-
+names_2013[names_2013$prop == max(names_2013$prop), "name"]
 
 # Write a function `most_popular_in_year` that takes in a year as a value and 
 # returns the most popular name in that year
-
+most_popular_in_a_year <- function(year2) {
+  names_in_year <- data.frame(names[names$year == year2, ])
+  most_pop_name <- names_in_year[names_in_year$prop == max(names_in_year$prop), "name"]
+  most_pop_name
+}
 
 # What was the most popular female name in 1994?
-
+most_popular_in_a_year(1994)
 
 # Write a function `number_in_million` that takes in a name and a year, and 
 # returns statistically how many babies out of 1 million born that year have 
 # that name. 
 # Hint: get the popularity percentage, and take that percentage out of 1 million.
-
+number_in_million <- function(name2, year2) {
+  name_prop <- names[names$name == name2 & names$year == year2, "prop"]
+  name_prop * 1000000
+}
 
 # How many babies out of 1 million had the name 'Laura' in 1995?
-
+number_in_million("Laura", 1995)
 
 # How many babies out of 1 million had your name in the year you were born?
 
